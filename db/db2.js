9acb6e6b198d6be44a232b9ad080063923f71587
Oscar Juarez <65364977+oscarjz-blue@users.noreply.github.com>
2024-06-30 20:37:13 -0300

    Preparacion de la conexion de base a la nube

--- db/db2.js
+++ db/db2.js
@@ -1,13 +1,30 @@
 const mysql = require("mysql2/promise");
+var poolCon = ''
+const PORT = process.env.PORT || 3000
+if (PORT == 3000) {
+    console.log("Creating connection pool...")
+    console.log('Conexion con base de datos local')
+    const poolLocal = mysql.createPool({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'movies_db'
+    })
+    poolCon = poolLocal
+}
 
-console.log("Creating connection pool...")
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'movies_db'
-})
+if (process.env.PORT == 10000) {
+    console.log("Creating connection pool...")
+    console.log('Conexion con base de datos en la nube')
+    const poolNube = mysql.createPool({
+        host: 'sql113.infinityfree.com',
+        user: 'if0_36804421',
+        password: 'cursonode',
+        database: 'if0_36804421_movies_db'
+    })
+    poolCon = poolNube
+}
 
-module.exports = pool;
+module.exports = poolCon;
 
 
